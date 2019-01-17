b69d08578badb043214837ffce24d14e55d07156
simple-keyboard <46766174+simple-keyboard@users.noreply.github.com>
2019-01-16 19:25:45 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# react-simple-keyboard-input-mask-demo
-Created with CodeSandbox
+<a href="https://github.com/hodgef/react-simple-keyboard" title="simple-keyboard repository"><img src="https://franciscohodge.com/project-pages/simple-keyboard/images/rsimplekeyboardl.png" width="250" align="right"></a>
+
+## Demo: react-simple-keyboard
+
+**Docs:**
+
+https://github.com/hodgef/react-simple-keyboard
+
+<a href="https://codesandbox.io/s/github/hodgef/demo_npm_react-simple-keyboard" title="run demo" target="_blank"><img src="https://franciscohodge.com/project-pages/simple-keyboard/images/rundemo200.png" align="center"></a>
---
+++ src/index.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Keyboard from "react-simple-keyboard";
+import inputMask from "simple-keyboard-input-mask";
+import "react-simple-keyboard/build/css/index.css";
+import "./index.css";
+
+class App extends Component {
+  state = {
+    layoutName: "default",
+    input: ""
+  };
+
+  onChange = input => {
+    this.setState({
+      input: input
+    });
+    console.log("Input changed", input);
+  };
+
+  onKeyPress = button => {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    //if (button === "{shift}" || button === "{lock}") this.handleShift();
+  };
+
+  handleShift = () => {
+    let layoutName = this.state.layoutName;
+
+    this.setState({
+      layoutName: layoutName === "default" ? "shift" : "default"
+    });
+  };
+
+  onChangeInput = event => {
+    let input = event.target.value;
+    this.setState(
+      {
+        input: input
+      },
+      () => {
+        this.keyboard.setInput(input);
+      }
+    );
+  };
+
+  inputStyle = {
+    width: "100%",
+    height: "100px",
+    padding: "10px",
+    fontSize: 20,
+    border: 0
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.input}
+          style={this.inputStyle}
+          placeholder={"(99) 9999-9999"}
+          //onChange={e => this.onChangeInput(e)}
+        />
+        <Keyboard
+          ref={r => (this.keyboard = r)}
+          theme={"hg-theme-default hg-layout-numeric numeric-theme"}
+          layoutName={this.state.layoutName}
+          onChange={input => this.onChange(input)}
+          onKeyPress={button => this.onKeyPress(button)}
+          disableCaretPositioning={true}
+          layout={{
+            default: ["1 2 3", "4 5 6", "7 8 9", "{//} 0 {//}", "{bksp}"],
+            shift: ["! / #", "$ % ^", "& * (", "{shift} ) +", "{bksp}"]
+          }}
+          inputMask={"(99) 9999-9999"}
+          modules={[inputMask]}
+        />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
